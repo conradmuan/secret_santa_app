413325f2d9bc135b4ddd534e4ba3c5f7f1bd9207
Conrad Muan <con.muan@gmail.com>
2013-01-06 00:54:22 -0500

    Views provided for: - GET / - GET /create_account - GET /list - POST /list

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 
 /*
- * GET home page.
+ * Routes in alphabetical order
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+var database = require('../lib/database');
+
+function createAccount(req, res){
+	res.render('create_account' , { title : "Create Account | Super Secret Santa App"});
+}
+
+function index(req, res) {
+	res.render('index' , { title : "Super Secret Santa App" });
+}
+
+function list(req, res){
+	/**
+	 * @todo: check if logged in
+	 */
+
+	res.render('list', { title: 'List Participants | Secret Santa App'});
+}
+
+function post_list(req, res){
+	/**
+	 * @todo: validation
+	 */
+	database.createAccount( req.body.name, req.body.password, req.body.email, function(){
+		res.render('list', { title: 'List Participants | Secret Santa App'});
+	});
+}
+
+module.exports = {
+	index : index,
+	createAccount : createAccount,
+	post_list : post_list,
+	list : list
+}
